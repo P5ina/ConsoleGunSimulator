f9eb8347a981025fbede8f4a3e56c1aed4518119
boris1592 <53943159+boris1592@users.noreply.github.com>
2020-12-16 21:18:53 +0300

    added config and updated main

--- gun.py
+++ gun.py
@@ -1,8 +1,37 @@
 from os import system
+from math import sin, pi, tau
+from json import load
+from time import sleep
 
-shootSound = "тататататататататататататататататататата"
-print(open("g.txt", "r").read())
+config = {}
+
+with open("config.json", "r") as json_file:
+	config = load(json_file)
+
+gun = []
+
+with open(config["gunfile"], "r") as gun_file:
+	gun = gun_file.read().split("\n")
+
+offset = config["offset"]
+speed = config["speed"]
+amplitude = config["amplitude"]
+delay = config["delay"]
+
+current = 0
+multiplier = 1
 
 while True:
+	# works only on windows
+	system("cls")
+	spaces = " " * (offset + current)
 
-	system("say "+shootSound)
+	for row in gun:
+		print(spaces + row)
+
+	current += speed * multiplier
+
+	if abs(current) >= amplitude:
+		multiplier *= -1
+
+	sleep(delay / 1000)
